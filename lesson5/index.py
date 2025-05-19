e9d0269f9cfeb53d7718d42adc3103756da30531
philliutwn <phil_liu_twn@hotmail.com>
2025-05-19 12:55:17 +0800

    Update

---
+++ lesson5/index.py
@@ -0,0 +1,21 @@
+from flask import Flask,
+
+print('Hello Flask!')
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    # return "<h1>Hello, World!</h1><h2>This is my first web page</h2>"
+    return render_template("index.html.jinja2")
+
+@app.route("/user")
+def user():
+    return "<h1>User!</h1><h2>This is my 2nd web page</h2>"
+
+@app.route("/Product")
+def product():
+    return "<h1>Product</h1><h2>This is my 3rd web page</h2>"
