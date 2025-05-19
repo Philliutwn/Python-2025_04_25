8996e3e458724aef24f0a9ed8523839f6f089eea
philliutwn <phil_liu_twn@hotmail.com>
2025-05-19 17:01:30 +0800

    Update

--- lesson5/index.py
+++ lesson5/index.py
@@ -12,10 +12,20 @@ def index():
     # return "<h1>Hello, World!</h1><h2>This is my first web page</h2>"
     return render_template("index.html.jinja2")
 
-@app.route("/user")
-def user():
-    return "<h1>User!</h1><h2>This is my 2nd web page</h2>"
+@app.route("/classes")
+def classes():
+    return render_template("classes.html.jinja2")
 
-@app.route("/Product")
-def product():
-    return "<h1>Product</h1><h2>This is my 3rd web page</h2>"
+
+@app.route("/news")
+def news():
+    return render_template("news.html.jinja2")
+
+@app.route("/traffic")
+def traffic():
+    return render_template("traffic.html.jinja2")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html.jinja2")
+
