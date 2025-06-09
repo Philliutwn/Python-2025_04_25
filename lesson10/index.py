3dfd3bdd6776400beff783a24600cb1bb590bf50
philliutwn <phil_liu_twn@hotmail.com>
2025-06-09 16:44:41 +0800

    update

--- lesson10/index.py
+++ lesson10/index.py
@@ -14,20 +14,42 @@ app = Flask(__name__)
 def index():
     return render_template("index.html.jinja2")
 
-@app.route("classes",defaults={"course_types":'一般課程'})
+@app.route("/classes",defaults={'course_types':'一般課程'})
 @app.route("/classes/<course_types>")
 def classes(course_types):
     # name = "Robert"
     # weekdays = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"]
+    print(course_types)
     conn = psycopg2.connect(conn_string)
     with conn.cursor() as cur:
         sql = "SELECT DISTINCT 課程類別 FROM 進修課程"
         cur.execute(sql)
-        kinds = cur.fetchall()
-        kinds = [kind[0] for kind in kinds]
-        print(kinds)
+        temps = cur.fetchall()
+        print('temps= ',temps)
+        kinds = [kind[0] for kind in temps]
+        print('kinds = ',kinds)
+        kinds.reverse()
+        print('kinds reverse =  ',kinds)
         
-    return render_template("classes.html.jinja2", kinds = kinds)
+        sql_course = f"""
+        SELECT
+            "課程名稱",
+            "老師",
+            "進修人數",
+            "報名開始日期",
+            "報名結束日期",
+            "課程內容",
+            "進修費用"
+        FROM
+            "進修課程"
+        WHERE
+            "課程類別" = '{course_types}'
+        LIMIT 6;
+        """
+        cur.execute(sql_course)
+        course_data = cur.fetchall()
+        print(course_data)
+    return render_template("classes.html.jinja2", kinds = kinds,course_data=course_data)
 
 @app.route("/new")
 def new():
